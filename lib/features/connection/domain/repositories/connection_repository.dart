af49d4107a1dc1a664d3fc223c8beed135369e44
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2026-05-04 13:48:55 +0800

    add: create ConnectionRepository interface for connection management

---
+++ lib/features/connection/domain/repositories/connection_repository.dart
@@ -0,0 +1,8 @@
+import '../../../../core/error/failures.dart';
+import '../entities/connection_entity.dart';
+
+abstract interface class ConnectionRepository {
+  Future<(ConnectionEntity?, AppFailure?)> loadSavedConnection();
+  Future<AppFailure?> saveConnection(ConnectionEntity connection);
+  Future<AppFailure?> testConnection(String ipAddress);
+}
